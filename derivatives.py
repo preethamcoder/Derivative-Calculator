7f3eb6283c021d6988e99d20be8055a29036dba0
Preetham Thelluri <46249739+preethampython101@users.noreply.github.com>
2020-12-28 02:08:34 -0500

    Update derivatives.py

--- derivatives.py
+++ derivatives.py
@@ -1,10 +1,8 @@
 from sympy.utilities.lambdify import lambdify
 from sympy import *
 from numpy import *
-from scipy.integrate import *
-import math, scipy
+import math
 import sympy as sym
-from scipy import integrate
 import numpy as np
 a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = sym.symbols('a b c d e f g h i j k l m n o p q r s t u v w x y z')
 print("Welcome to the DERIVATIVE calculator. This calculator is capable of differentiating single and multi-variable functions. Don't worry about the conditions, just enter the functions and variables!")
@@ -12,8 +10,6 @@ print("Enter the function you want to differentiate along the differentiating va
 aba = input("Function now:\n")
 diff_var = input("Variable now:\n")
 abc = aba.replace("^", "**").replace("sin", "sym.sin").replace("cos", "sym.cos").replace("ln", "sym.ln").replace("log", "sym.log")
-#print(aba)
-# Define function g
 def g(x):
     func = eval(abc)
     return func
